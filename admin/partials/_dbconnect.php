4179f3eec079c03614c23faccab01909f7030127
Gaurav Rajput <137691243+GauravRajput123@users.noreply.github.com>
2024-04-20 11:16:00 +0530

    Add files via upload

---
+++ admin/partials/_dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$port = "5432"; 
+$dbname = "opd";
+$user = "postgres"; 
+$password = "Rahulwagh@2211"; 
+
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
+
+$conn = pg_connect($connection_string);
+
+if (!$conn) {
+    die("Error: Could not connect to the database.");
+}
+
+?>
+
